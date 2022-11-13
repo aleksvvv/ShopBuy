022fe0b0e034cbd129770fa0440a489e0745bf51
aleksvvv <adm-shop-001@yandex.ru>
2022-11-13 12:20:12 +0300

    Coroutine.Add all metod DB. 13.11.2022.

--- app/src/main/java/com/example/shopbuy/data/ShopListRepositoryImpl.kt
+++ app/src/main/java/com/example/shopbuy/data/ShopListRepositoryImpl.kt
@@ -1,13 +1,20 @@
 package com.example.shopbuy.data
 
+import android.app.Application
 import androidx.lifecycle.LiveData
+import androidx.lifecycle.MediatorLiveData
 import androidx.lifecycle.MutableLiveData
+import androidx.lifecycle.Transformations
 import com.example.shopbuy.domain.ShopItem
 import com.example.shopbuy.domain.ShopListRepository
 import kotlin.random.Random
 
-object ShopListRepositoryImpl : ShopListRepository {
+class ShopListRepositoryImpl(application: Application) : ShopListRepository {
     private val shopListLD = MutableLiveData<List<ShopItem>>()
+    private val mapper = ShopMapper()
+
+    private val shopListDao = AppDatabase.getInstance(application).shopListDao()
+
     // private val shopList = mutableListOf<ShopItem>()
 
 //    private val shopList = sortedSetOf<ShopItem>(object : Comparator<ShopItem>{
@@ -16,53 +23,71 @@ object ShopListRepositoryImpl : ShopListRepository {
 //
 //            }
 
-    private val shopList = sortedSetOf<ShopItem>({ p0, p1 -> p0.id.compareTo(p1.id) })
-
+    //    private val shopList = sortedSetOf<ShopItem>({ p0, p1 -> p0.id.compareTo(p1.id) })
+//
     private var autoIncrementId = 0
-
-    init {
-        for (i in 0 until 5) {
-            val item = ShopItem("name $i", i, Random.nextBoolean())
-            addShopItem(item)
-        }
-    }
+//
+//    init {
+//        for (i in 0 until 5) {
+//            val item = ShopItem("name $i", i, Random.nextBoolean())
+//            addShopItem(item)
+//        }
+//    }
 
     override fun addShopItem(shopItem: ShopItem) {
-        //проверка, если добавлятся новый элемент
-        if (shopItem.id == ShopItem.UNDEFENDED_ID) {
-            shopItem.id = autoIncrementId++
-        }
-        //это используется для редактирования
-        shopList.add(shopItem)
-        //обновляем ливдату
-        updateList()
+        shopListDao.addShopItem(mapper.shopItemEntryToShopItemDb(shopItem))
+//        //проверка, если добавлятся новый элемент
+//        if (shopItem.id == ShopItem.UNDEFENDED_ID) {
+//            shopItem.id = autoIncrementId++
+//        }
+//        //это используется для редактирования
+//        shopList.add(shopItem)
+//        //обновляем ливдату
+//        updateList()
     }
 
     override fun deleteShopItem(shopItem: ShopItem) {
-        shopList.remove(shopItem)
-        //обновляем ливдату
-        updateList()
+        shopListDao.delShopItem(shopItem.id)
+
+//        shopList.remove(shopItem)
+//        //обновляем ливдату
+//        updateList()
     }
 
     override fun editShopItem(shopItem: ShopItem) {
-        val oldShopItem = getShopItemDetails(shopItem.id)
-        deleteShopItem(oldShopItem)
-        addShopItem(shopItem)
+        shopListDao.addShopItem(mapper.shopItemEntryToShopItemDb(shopItem))
+//        val oldShopItem = getShopItemDetails(shopItem.id)
+//        deleteShopItem(oldShopItem)
+//        addShopItem(shopItem)
     }
 
     override fun getShopItemDetails(shopItemId: Int): ShopItem {
-        return shopList.find {
-            it.id == shopItemId
-        } ?: throw RuntimeException("Элемент $shopItemId не найден")
+        val dbModel = shopListDao.getShopItem(shopItemId)
+        return mapper.shopItemDbToShopItemEntry(dbModel)
+//        return shopList.find {
+//            it.id == shopItemId
+//        } ?: throw RuntimeException("Элемент $shopItemId не найден")
 
     }
 
-    override fun getShopItemList(): LiveData<List<ShopItem>> {
-        return shopListLD
+//    override fun getShopItemList(): LiveData<List<ShopItem>> {
+//        return MediatorLiveData<List<ShopItem>>().apply {
+//            addSource(shopListDao.getShopList()) {
+//                value = mapper.shopListDbToShopListEntry(it)
+//            }
+//        }
+//    }
+    //Второй вариант, более короткий
+override fun getShopItemList(): LiveData<List<ShopItem>> {
+   return Transformations.map(
+        shopListDao.getShopList()){
+        mapper.shopListDbToShopListEntry(it)
     }
+}
 
-    private fun updateList() {
-        //в лив дату присваивмаем копию списка
-        shopListLD.value = shopList.toList()
-    }
+
+//    private fun updateList() {
+//        //в лив дату присваивмаем копию списка
+//        shopListLD.value = shopList.toList()
+//    }
 }
